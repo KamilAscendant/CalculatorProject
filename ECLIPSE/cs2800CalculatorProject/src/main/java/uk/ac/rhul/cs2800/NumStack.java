c2dc7ef44975a6be1d6539f4117936501060e3a5
Abrar Kamil (2021) ZKAC152 <zkac152@live.rhul.ac.uk>
2022-12-03 02:28:31 +0000

    Worked on numStack as a facade, working on other Stack facades

--- ECLIPSE/cs2800CalculatorProject/src/main/java/uk/ac/rhul/cs2800/NumStack.java
+++ ECLIPSE/cs2800CalculatorProject/src/main/java/uk/ac/rhul/cs2800/NumStack.java
@@ -1,5 +1,11 @@
 package uk.ac.rhul.cs2800;
 
 public class NumStack {
-
+  private Stack stackNum = new Stack();
+  private float numEntered;
+  
+  public void push(Entry numEntered) {
+    stackNum.push(numEntered);
+  }
+  
 }
