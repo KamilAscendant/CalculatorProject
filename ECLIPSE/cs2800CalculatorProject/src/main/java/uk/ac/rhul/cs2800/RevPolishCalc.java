20a5e403df8a3cb7a1534ed1f17ab0f9a452cb45
Abrar Kamil (2021) ZKAC152 <zkac152@live.rhul.ac.uk>
2022-12-15 20:51:35 +0000

    RevPolishCalc now throws an Exception on being asked to evaluate an empty String.

--- ECLIPSE/cs2800CalculatorProject/src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
+++ ECLIPSE/cs2800CalculatorProject/src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -1,7 +1,12 @@
 package uk.ac.rhul.cs2800;
 
+import uk.ac.rhul.cs2800.InvalidExpressionException;
+
 public class RevPolishCalc {
   public float evaluate(String expression) throws InvalidExpressionException{
+    if (expression == null || expression.equalsIgnoreCase("")) {
+      throw new InvalidExpressionException("There is no expression to evaluate.");
+    }
     return 99.7f;
   }
 }
