aff19af78915d7324e9d4ee43f66ce4790653fd8
Abrar Kamil (2021) ZKAC152 <zkac152@live.rhul.ac.uk>
2022-12-15 21:22:37 +0000

    RevPolishCalc can now evaluate a String containing only a number and successfully returns the float value.

--- ECLIPSE/cs2800CalculatorProject/src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
+++ ECLIPSE/cs2800CalculatorProject/src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -1,12 +1,26 @@
 package uk.ac.rhul.cs2800;
 
+import java.util.Scanner;
 import uk.ac.rhul.cs2800.InvalidExpressionException;
 
 public class RevPolishCalc {
+  private NumStack values;
+  private float answer;
+  
   public float evaluate(String expression) throws InvalidExpressionException{
     if (expression == null || expression.equalsIgnoreCase("")) {
       throw new InvalidExpressionException("There is no expression to evaluate.");
     }
-    return 99.7f;
+    else {
+      Scanner equation = new Scanner(expression);
+      this.values = new NumStack();
+      while(equation.hasNext()) {
+        if(equation.hasNextFloat()) {
+          this.values.push(equation.nextFloat());
+        }
+      }
+      this.answer = this.values.pop();
+      return(this.answer);
+    }
   }
 }
--- ECLIPSE/cs2800CalculatorProject/src/test/java/uk/ac/rhul/cs2800/TestRevPolish.java
+++ ECLIPSE/cs2800CalculatorProject/src/test/java/uk/ac/rhul/cs2800/TestRevPolish.java
@@ -14,9 +14,21 @@ class TestRevPolish {
     rp = new RevPolishCalc();
   }
   
+  //Test1
+  //Checks to see if RevPolishCalc throws an exception upon being asked to evaluate an empty String
   @Test
-  void test() {
-    //assertThrows(rp.evaluate("179.4 2 10 *+");
+  void testEmpty() {
     assertThrows(InvalidExpressionException.class, () -> rp.evaluate(""));
+  }
+  
+  //Test2
+  //Checks what happens when asked to evaluate a String with only an float
+  @Test
+  void testFloat() {
+    try {
+      assertEquals(0.0f, rp.evaluate("0"), "Checking to see if it can read float value of a string");
+    } catch (InvalidExpressionException e) {
+      e.printStackTrace();
+    }
   }
 }
