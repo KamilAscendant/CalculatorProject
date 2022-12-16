6159f7defd5dd4e343abceccbaf7576d56ee1660
Abrar Kamil (2021) ZKAC152 <zkac152@live.rhul.ac.uk>
2022-12-16 00:16:21 +0000

    This branch is to work on the StandardCalc model, which will be implementing the logic from the RevPolishCalc.

---
+++ ECLIPSE/cs2800CalculatorProject/src/main/java/uk/ac/rhul/cs2800/InvalidExpressionException.java
@@ -0,0 +1,8 @@
+package uk.ac.rhul.cs2800;
+
+public class InvalidExpressionException extends Exception {
+  
+  private static final long serialVersionUID = 2L;
+  
+  public InvalidExpressionException(String string) {}
+}
--- ECLIPSE/cs2800CalculatorProject/src/main/java/uk/ac/rhul/cs2800/StandardCalc.java
+++ ECLIPSE/cs2800CalculatorProject/src/main/java/uk/ac/rhul/cs2800/StandardCalc.java
@@ -1,5 +1,7 @@
 package uk.ac.rhul.cs2800;
 
 public class StandardCalc {
-  
+  public float evaluate(String expression) throws InvalidExpressionException {
+    return 0.0f;
+  }
 }
