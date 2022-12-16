8ac40a28c6ec687c12da244c6ce058bbcb75fe43
Abrar Kamil (2021) ZKAC152 <zkac152@live.rhul.ac.uk>
2022-12-16 00:36:54 +0000

    Throws an exception when a null or empty String is evaluated by StandardCalc.

--- ECLIPSE/cs2800CalculatorProject/src/main/java/uk/ac/rhul/cs2800/StandardCalc.java
+++ ECLIPSE/cs2800CalculatorProject/src/main/java/uk/ac/rhul/cs2800/StandardCalc.java
@@ -1,7 +1,19 @@
 package uk.ac.rhul.cs2800;
 
 public class StandardCalc {
+  
+  private RevPolishCalc rp;
+  private StrStack rpEvaluates;
+  
+  public StandardCalc() {
+    rp = new RevPolishCalc();
+    rpEvaluates = null;
+  }
+  
   public float evaluate(String expression) throws InvalidExpressionException {
-    return 0.0f;
+    if (expression == null || expression == ("")) {
+      throw new InvalidExpressionException("Invalid Expression");
+    }
+    return 0f;
   }
 }
