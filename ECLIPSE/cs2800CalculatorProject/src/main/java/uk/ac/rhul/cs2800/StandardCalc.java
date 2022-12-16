961de6605b58b960a85bf5e90a437b79dbdf2791
Abrar Kamil (2021) ZKAC152 <zkac152@live.rhul.ac.uk>
2022-12-16 01:06:17 +0000

    The StandardCalc can now take a single number input. The underlying logic for converting infix to postfix has also begun to be constructed.

--- ECLIPSE/cs2800CalculatorProject/src/main/java/uk/ac/rhul/cs2800/StandardCalc.java
+++ ECLIPSE/cs2800CalculatorProject/src/main/java/uk/ac/rhul/cs2800/StandardCalc.java
@@ -1,19 +1,39 @@
 package uk.ac.rhul.cs2800;
 
+import java.util.Scanner;
+
 public class StandardCalc {
   
   private RevPolishCalc rp;
-  private StrStack rpEvaluates;
+  private StrStack rpEvaluator;
+  private OpStack operators;
+  private StringBuilder evaluates;
   
   public StandardCalc() {
     rp = new RevPolishCalc();
-    rpEvaluates = null;
+    rpEvaluator = null;
+    operators = null;
+    evaluates = null;
+    
   }
   
   public float evaluate(String expression) throws InvalidExpressionException {
     if (expression == null || expression == ("")) {
       throw new InvalidExpressionException("Invalid Expression");
+    } 
+    Scanner equation = new Scanner(expression);
+    this.rpEvaluator = new StrStack();
+    this.operators = new OpStack();
+    this.evaluates = new StringBuilder();
+    while(equation.hasNext()) {
+      rpEvaluator.push(equation.next());
     }
-    return 0f;
+    equation.close();
+    while(rpEvaluator.size() != 0) {
+      evaluates.append(String.valueOf(rpEvaluator.pop()));
+    }
+    String finalEquation = evaluates.toString();
+    float result = rp.evaluate(finalEquation);
+    return result;
   }
 }
